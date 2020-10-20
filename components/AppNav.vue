740a17f651aa3fd67934dc96203b516d5780765b
caryl wyatt <caryl.e.wyatt@gmail.com>
2020-10-20 10:58:32 -0500

    added menu data, updated styles

--- components/AppNav.vue
+++ components/AppNav.vue
@@ -11,87 +11,25 @@
             <font-awesome-icon v-if="!isOpen" icon="bars" class="fa-2x fill-current"/>
             <font-awesome-icon v-if="isOpen" icon="times" class="fa-2x fill-current"/>
           </button>
+          <!-- <button v-if="isOpen" @click="isOpen = false" class="fixed inset-0 h-full w-full bg-black opacity-50 cursor-default"></button> -->
         </div>
       </div>
-      <div :class="isOpen ? 'block' : 'hidden'" class="px-5 lg:flex lg:items-center">
-
-        <!-- <div class="dropdown px-4"> -->
-        <!--   <button class="py-2 w-full text-white flex items-center justify-between">Research Tools -->
-        <!--     <font-awesome-icon icon="angle-down" class="fill-current" /> -->
-        <!--   </button> -->
-          <!--<div class="dropdown-content bg-white py-2 ">
-            <nuxt-link to="/" class="block px-4 py-2 text-red-800 hover:bg-gray-200">Scout</nuxt-link>
-            <nuxt-link to="/" class="block px-4 py-2 text-red-800 hover:bg-gray-200">Libraries Catalog</nuxt-link>
-            <nuxt-link to="/" class="block px-4 py-2 text-red-800 hover:bg-gray-200">Databases</nuxt-link>
-            <nuxt-link to="/" class="block px-4 py-2 text-red-800 hover:bg-gray-200">Google Scholar</nuxt-link>
-            <nuxt-link to="/" class="block px-4 py-2 text-red-800 hover:bg-gray-200">Research Guides</nuxt-link>
-            <nuxt-link to="/" class="block px-4 py-2 text-red-800 hover:bg-gray-200">E-Journals</nuxt-link>
-            <nuxt-link to="/" class="block px-4 py-2 text-red-800 hover:bg-gray-200">Distance Education</nuxt-link>
-            <nuxt-link to="/" class="block px-4 py-2 text-red-800 hover:bg-gray-200">Government Information, Statistics, and Data</nuxt-link>
-            <nuxt-link to="/" class="block px-4 py-2 text-red-800 hover:bg-gray-200">Music Library</nuxt-link>
-            <nuxt-link to="/" class="block px-4 py-2 text-red-800 hover:bg-gray-200">Special Collections</nuxt-link>
-            <nuxt-link to="/" class="block px-4 py-2 text-red-800 hover:bg-gray-200">Citation Managers</nuxt-link>
-            <nuxt-link to="/" class="block px-4 py-2 text-red-800 hover:bg-gray-200">Research Data Services</nuxt-link>
-          </div>-->
-        <!-- </div> -->
-
-        <!-- <div class="dropdown px-4 h-full flex"> -->
-        <!--   <button class="py-2 w-full text-white flex items-center justify-between">Using the Library -->
-        <!--     <font-awesome-icon icon="angle-down" /> -->
-        <!--   </button> -->
-          <!--<div class="dropdown-content">
-            <nuxt-link to="/">Find a Place to Study</nuxt-link>
-            <nuxt-link to="/">Borrow, Renew & Course Reserves</nuxt-link>
-            <nuxt-link to="/">Digital Humanities Center</nuxt-link>
-            <nuxt-link to="/">Accessibility</nuxt-link>
-            <nuxt-link to="/">Computers and Equipment</nuxt-link>
-            <nuxt-link to="/">Interlibrary Loan</nuxt-link>
-            <nuxt-link to="/">Sanford Media Center</nuxt-link>
-            <nuxt-link to="/">Information for Faculty</nuxt-link>
-            <nuxt-link to="/">Classes and Workshops</nuxt-link>
-            <nuxt-link to="/">Information for Students</nuxt-link>
-            <nuxt-link to="/">Library Software</nuxt-link>
-          </div>-->
-        <!-- </div> -->
-
-        <!-- <div class="dropdown px-4 h-full flex"> -->
-        <!--     <button class="py-2 w-full text-white flex items-center justify-between">About -->
-        <!--       <font-awesome-icon icon="angle-down" /> -->
-        <!--     </button> -->
-            <!--<div class="dropdown-content">
-              <nuxt-link to="/">Hours</nuxt-link>
-              <nuxt-link to="/">Directory</nuxt-link>
-              <nuxt-link to="/">News and Events</nuxt-link>
-              <nuxt-link to="/">Policies</nuxt-link>
-              <nuxt-link to="/">Libraries Annex</nuxt-link>
-              <nuxt-link to="/">Support UA Libraries</nuxt-link>
-              <nuxt-link to="/">Social Media</nuxt-link>
-              <nuxt-link to="/">About the Libraries</nuxt-link>
-            </div>-->
-          <!-- </div> -->
-          <!-- @click="selected = index" -->
+      <div :class="isOpen ? 'block' : 'hidden'" class="lg:flex lg:items-center">
           <div 
             v-for="(item, index) in items" 
             :key="item.heading" 
             class="dropdown px-4 h-full flex flex-col">
-            <button @click="setSelectedIndex(index)" class="py-2 w-full text-white flex items-center justify-between">
+            <button @click="setSelectedIndex(index)" class="py-2 w-full h-full text-white flex items-center justify-between">
               <span>{{ item.heading }}</span>
-              <font-awesome-icon v-if="index !== selected" icon="angle-down" />
-              <font-awesome-icon v-if="index == selected" icon="times" />
+              <font-awesome-icon v-if="index !== selected" icon="angle-down" class="fa-fw m-1"/>
+              <font-awesome-icon v-if="index == selected" icon="times" class="fa-fw m-1"/>
             </button>
-            <!-- :class="{active:index == selected}" -->
             <div :class="{active:index == selected}" class="lg:absolute lg:left-0 dropdown-content flex flex-col bg-white lg:mt-20 hidden">
-              <nuxt-link v-for="link in item.links" :key="link.title" class="px-4 py-2  text-red-800 hover:bg-gray-200" to="/"><font-awesome-icon :icon="link.icon" class="fill-current" /> {{ link.title }}</nuxt-link>
-              <!-- <nuxt-link class="px-4 py-2 text-red-800 hover:bg-gray-200" to="/"><font-awesome-icon icon="comment" class="fill-current" /> Liaisons</nuxt-link> -->
-              <!-- <nuxt-link class="px-4 py-2 text-red-800 hover:bg-gray-200" to="/"><font-awesome-icon icon="envelope" class="fill-current" /> Run into Website Issues?</nuxt-link> -->
-              <!-- <nuxt-link class="px-4 py-2 text-red-800 hover:bg-gray-200" to="/"><font-awesome-icon icon="wifi" class="fill-current" /> Connect to a Wireless Network</nuxt-link> -->
-              <!-- <nuxt-link class="px-4 py-2 text-red-800 hover:bg-gray-200" to="/"><font-awesome-icon icon="comments" class="fill-current" /> Request a Research Consultation</nuxt-link> -->
-              <!-- <nuxt-link class="px-4 py-2 text-red-800 hover:bg-gray-200" to="/"><font-awesome-icon icon="sign-in-alt" class="fill-current" /> Report a Login Problem</nuxt-link> -->
-              <!-- <nuxt-link class="px-4 py-2 text-red-800 hover:bg-gray-200" to="/"><font-awesome-icon icon="question-circle" class="fill-current" /> Tutorials and Instructional Videos</nuxt-link> -->
+              <nuxt-link v-for="link in item.links" :key="link.title" class="px-4 py-2  text-red-800 hover:bg-gray-200" to="/"><font-awesome-icon :icon="[link.iconClass, link.icon]" class="fa-fw fill-current" /> {{ link.title }}</nuxt-link>
             </div>
           </div>
-          <nuxt-link class="block py-2 text-white px-4 h-full flex" to="/">Giving</nuxt-link>
-          <nuxt-link v-if="!isOpen" class="block py-2 text-white px-4 h-full flex" to="/"><span class="h5"><font-awesome-icon icon="user" /></span></nuxt-link>
+          <nuxt-link class="py-2 text-white px-4 h-full flex items-center" to="/">Giving</nuxt-link>
+          <nuxt-link class="sm:hidden py-2 text-white px-4 h-full lg:flex items-center" to="/"><span class="h5"><font-awesome-icon icon="user" /></span></nuxt-link>
       </div> 
     </nav>
   </header>
@@ -135,6 +73,27 @@
                 title: 'Databases',
                 icon: 'database',
               },
+              {
+                title: 'Distance Education',
+                icon: 'globe',
+              },
+              {
+                title: 'Citation Managers',
+                icon: 'folder',
+              },
+              {
+                title: 'Google Scholar',
+                icon: 'google',
+                iconClass: 'fab'
+              },
+              {
+                title: 'Government Information, Statistics, Data',
+                icon: 'chart-bar',
+              },
+              {
+                title: 'Research Data Services',
+                icon: 'archive',
+              },
             ]
           }, 
           {
@@ -164,8 +123,100 @@
                 title: 'Information for Students',
                 icon: 'pencil-alt',
               },
+              {
+                title: 'Digital Humanities Center',
+                icon: 'tachometer-alt',
+              },
+              {
+                title: 'Sanford Media Center',
+                icon: 'cubes',
+              },
+              {
+                title: 'Library Software',
+                icon: 'pencil-alt',
+              },
+              {
+                title: 'Accessibility',
+                icon: 'accessible-icon',
+                iconClass: 'fab'
+              },
+              {
+                title: 'Information for Faculty',
+                icon: 'graduation-cap',
+              },
             ]
           }, 
+          {
+            heading: 'About',
+            links: [
+              
+              { 
+                title: 'Hours',
+                icon: 'clock'
+              },
+              { 
+                title: 'Directory',
+                icon: 'users'
+              },
+              { 
+                title: 'News and Events',
+                icon: 'newspaper'
+              },
+              { 
+                title: 'Policies',
+                icon: 'sitemap'
+              },
+              { 
+                title: 'Libraries Annex', 
+                icon: 'building'
+              },
+              { 
+                title: 'Support UA Libraries',
+                icon: 'gift'
+              },
+              {
+                title: 'Social Media', 
+                icon: 'share-alt'
+              },
+              {
+                title: 'About the Libraries',
+                icon: 'university'
+              },
+            ]
+          },
+          {
+            heading: 'Library Help',
+            links: [
+              {
+                title: 'Ask a Librarian',
+                icon: 'question-circle'
+              },
+              {
+                title: 'Liaisons',
+                icon: 'comment'
+              },
+              {
+                title: 'Run into a Website Issue?',
+                icon: 'envelope'
+              },
+              {
+                title: 'Connect to a Wireless Network',
+                icon: 'wifi'
+              },
+              {
+                title: 'Request a Research Consultation',
+                icon: 'comments'
+              },
+              {
+                title: 'Report a Login Problem',
+                icon: 'sign-in-alt'
+              },
+              {
+                title: 'Tutorials and Instructional Videos',
+                icon: 'question-circle'
+              },
+            ]
+          }
           
         ]
       }
