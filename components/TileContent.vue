0dcddd6f3cf530310ad6850f19cfee4fab8691e0
caryl wyatt <caryl.e.wyatt@gmail.com>
2020-11-04 14:07:06 -0600

    update json object for new API data

--- components/TileContent.vue
+++ components/TileContent.vue
@@ -3,7 +3,7 @@
   <font-awesome-icon :icon="[link.icon_class, link.icon]" class="mt-1 mr-2 text-2xl text-gray-600 fill-current fa-fw" /> 
     <span class="ml-2">
       <h4 class="text-xl leading-tight">{{ link.title }}</h4>
-      <p class="text-black">{{ link.post_content }}</p> 
+      <p class="text-black">{{ link.description }}</p> 
       <ul class="mt-1">
         <li v-for="subLink in link.child_items" :key="subLink.ID" class="group">
           <a 
