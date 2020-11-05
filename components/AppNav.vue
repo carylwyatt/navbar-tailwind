f0233c448f4ae40b5304ffc3f6d4159c4341be44
caryl wyatt <caryl.e.wyatt@gmail.com>
2020-11-05 11:48:50 -0600

    fixed mobile styles for dropdown menu

--- components/AppNav.vue
+++ components/AppNav.vue
@@ -5,9 +5,9 @@
         <div>
           <nuxt-link to="/"><img class="h-16 my-2" src="https://www.lib.ua.edu/wp-content/themes/roots-ualib/assets/img/ualib-logo-capstone.png" alt="University Libraries Logo"></nuxt-link>
         </div>
-        <div class="flex items-center h-full lg:hidden">
-          <nuxt-link class="h-full px-4 text-white fa-lg hover:bg-white hover:text-red-800" to="/"><font-awesome-icon icon="user"/></nuxt-link>
-          <button @click="isOpen = !isOpen" type="button" class="flex h-full text-white hover:bg-white hover:text-red-800">
+        <div class="flex items-center h-20 lg:hidden">
+          <nuxt-link class="flex items-center h-full px-5 text-white fa-lg hover:bg-white hover:text-red-800" to="/"><font-awesome-icon icon="user"/></nuxt-link>
+          <button @click="isOpen = !isOpen" type="button" class="flex items-center h-full px-5 text-white hover:bg-white hover:text-red-800">
             <font-awesome-icon v-if="!isOpen" icon="bars" class="fill-current fa-2x"/>
             <font-awesome-icon v-if="isOpen" icon="times" class="fill-current fa-2x"/>
           </button>
